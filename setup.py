d6d3492f77964e58c20d8ce41decf67338bacc79
Murad042 <alemdarpolad72@gmail.com>
2022-01-25 21:11:29 +0400

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='it is a data science project',
+    author='Murad042',
+    license='MIT',
+)
